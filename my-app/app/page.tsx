6287e3005878ada1c717ee4cb13165cd0c301017
Thomas Danielsson <69715206+DanielssonThomas@users.noreply.github.com>
2023-09-05 13:45:05 +0200

    add: login button

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -14,5 +14,18 @@ export default async function Index() {
     data: { user },
   } = await supabase.auth.getUser();
 
-  return <div className=""></div>;
+  return (
+    <div className="">
+      <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
+        <div className="w-full max-w-4xl flex justify-center items-center p-3 text-sm text-foreground">
+          <Link
+            href="/login"
+            className="py-2 px-4 rounded-md no-underline bg-btn-background hover:bg-btn-background-hover"
+          >
+            Login
+          </Link>
+        </div>
+      </nav>
+    </div>
+  );
 }
