e9e04ec860008c3a196d15f26a57eeb12b221d2b
Thomas Danielsson <69715206+DanielssonThomas@users.noreply.github.com>
2023-09-05 13:41:01 +0200

    add: subscription form in Admin

---
+++ my-app/app/Admin/SubscriptionForm/page.tsx
@@ -0,0 +1,35 @@
+import { createServerActionClient } from "@supabase/auth-helpers-nextjs";
+import { cookies } from "next/headers";
+export default function AdminForm() {
+  const handleForm = async (formData: FormData) => {
+    "use server";
+    const supabase = createServerActionClient({ cookies });
+    const name = String(formData.get("name"));
+    const price = Number(formData.get("price"));
+    const trial_length_days = Number(formData.get("trial_length_days"));
+
+    const { error } = await supabase.from("subscriptions").insert({
+      name,
+      price,
+      trial_length_days,
+    });
+
+    if (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <div>
+      <form action={handleForm} method="POST">
+        <label htmlFor="name">Enter name:</label>
+        <input type="text" name="name" />
+        <label htmlFor="price">Enter price:</label>
+        <input type="number" name="price" />
+        <label htmlFor="trial_length_days">Enter free trial length:</label>
+        <input type="number" name="trial_length_days" />
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
+}
