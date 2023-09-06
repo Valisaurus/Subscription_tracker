c593ebcb5c1f011c55acc43cc28c5953fcdd94c5
Thomas Danielsson <69715206+DanielssonThomas@users.noreply.github.com>
2023-09-06 13:58:36 +0200

    add: style on lightswitch

--- my-app/components/LightSwitch.tsx
+++ my-app/components/LightSwitch.tsx
@@ -6,7 +6,7 @@ type LightSwitchProps = {
 const LightSwitch = ({ lightMode, setLightMode }: LightSwitchProps) => {
   return (
     <button
-      className="absolute top-1 right-1 dark:text-white text-black"
+      className="absolute top-4 right-4 dark:text-white text-black"
       onClick={() => {
         setLightMode(!lightMode);
         console.log(lightMode);
