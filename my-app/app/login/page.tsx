3aea07b892825e040cae32eb554b807462ea8312
Thomas Danielsson <69715206+DanielssonThomas@users.noreply.github.com>
2023-09-06 13:58:19 +0200

    add: terms&conditions

--- my-app/app/login/page.tsx
+++ my-app/app/login/page.tsx
@@ -2,23 +2,34 @@
 import { useState } from "react";
 import LightSwitch from "@/components/LightSwitch";
 import LoginForm from "@/components/LoginForm";
+import Logo from "@/components/Logo";
+import TermsAndConditions from "@/components/TermsAndConditions";
+
 export default function Login() {
   const [lightMode, setLightMode] = useState<boolean>(false);
+  const [renderTopLogo, setRenderTopLogo] = useState<boolean>(false);
   return (
     <div className={`${lightMode ? "dark" : ""}`}>
       <div className="flex-1 flex flex-col w-full justify-center gap-2 bg-white dark:bg-black">
+        {renderTopLogo ? <Logo /> : <div></div>}
         <LightSwitch lightMode={lightMode} setLightMode={setLightMode} />
         <div className="flex flex-col justify-around items-center h-screen py-24">
-          <div className="">
-            <h2 className="text-6xl text-center text-black dark:text-white">
-              CTRL+
-            </h2>
-            <i className="text-xs text-center text-black dark:text-white">
-              Take ctrl of your streaming subscriptions
-            </i>
-          </div>
-          <div>
-            <LoginForm />
+          <div className="flex flex-col justify-around items-center">
+            {!renderTopLogo ? (
+              <div>
+                <h2 className="text-6xl text-center text-black dark:text-white">
+                  CTRL+
+                </h2>
+                <i className="text-xs text-center text-black dark:text-white">
+                  Take ctrl of your streaming subscriptions
+                </i>
+              </div>
+            ) : (
+              <div></div>
+            )}
+            <div>
+              <LoginForm setRenderTopLogo={setRenderTopLogo} />
+            </div>
           </div>
         </div>
       </div>
---
+++ my-app/components/TermsAndConditions.tsx
@@ -0,0 +1,32 @@
+type TermsAndConditionsProps = {
+  email: string;
+  password: string;
+};
+
+const TermsAndConditions = ({ email, password }: TermsAndConditionsProps) => {
+  return (
+    <div className="w-full flex flex-col justify-around items-center p-[70px]">
+      <div className="w-[250] h-[529px] border-black border-4 dark:border-white text-black dark:text-white p-[20px] overflow-scroll text-center">
+        TERMS TERMS TERMS TERMS TERMS TERMS TERMS TERMS TERMS TERMS TERMS TERMS
+        TERMS TERMS TERMS TERMS TERMS TERMS TERMS TERMS TERMS TERMS TERMS TERMS
+        TERMS TERMS TERMS TERMS TERMS TERMS TERMS TERMS TERMS TERMS TERMS TERMS
+        TERMS TERMS TERMS TERMS TERMS TERMS TERMS TERMS TERMS TERMS TERMS TERMS
+        TERMS TERMS TERMS TERMS TERMS TERMS TERMS TERMS TERMS TERMS TERMS TERMS
+        TERMS TERMS TERMS TERMS TERMS TERMS TERMS TERMS TERMS TERMS TERMS TERMS
+        TERMS TERMS TERMS TERMS TERMS TERMS TERMS TERMS TERMS TERMS TERMS TERMS
+        TERMS TERMS TERMS TERMS TERMS TERMS TERMS TERMS TERMS TERMS TERMS TERMS
+        TERMS TERMS TERMS TERMS TERMS TERMS TERMS TERMS TERMS TERMS TERMS TERMS
+        TERMS TERMS TERMS TERMS TERMS TERMS TERMS TERMS TERMS TERMS TERMS TERMS
+        TERMS TERMS TERMS TERMS TERMS TERMS TERMS TERMS
+      </div>
+      <form action="/auth/sign-up" method="post">
+        <input type="hidden" name="email" value={email} />
+        <input type="hidden" name="password" value={password} />
+        <button className="w-[200px] h-[40px] border-black border-2 dark:border-white text-black dark:text-white mt-[30px]">
+          Accept terms&conditions
+        </button>
+      </form>
+    </div>
+  );
+};
+export default TermsAndConditions;
