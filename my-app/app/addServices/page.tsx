bb94cd8f5e2accff576f11c34b2137fd106c78d5
Thomas Danielsson <69715206+DanielssonThomas@users.noreply.github.com>
2023-09-07 13:52:08 +0200

    add: services for form

--- my-app/app/addServices/page.tsx
+++ my-app/app/addServices/page.tsx
@@ -1,9 +1,11 @@
 import ServiceForm from "@/components/ServiceForm";
 import { createServerActionClient } from "@supabase/auth-helpers-nextjs";
 import { cookies } from "next/headers";
+import { Database } from "@/lib/database.types";
+import { Subscription } from "@supabase/supabase-js";
 
 const AddServices = async () => {
-  const supabase = createServerActionClient({ cookies });
+  const supabase = createServerActionClient<Subscription>({ cookies });
   const {
     data: { user },
   } = await supabase.auth.getUser();
@@ -12,7 +14,11 @@ const AddServices = async () => {
     .from("subscriptions")
     .select("name, price, subscriptions_users (user_id, subscription_id)");
 
-  const services: string[][] | undefined = data?.map((service) => service.name);
+  const services: { name: string; price: number }[] | undefined = data?.map(
+    (service: { name: string; price: number }) => {
+      return { name: service?.name, price: service?.price };
+    }
+  );
 
   data?.forEach((service) => {
     console.log(`Service: ${service.name}`);
--- my-app/components/ServiceForm.tsx
+++ my-app/components/ServiceForm.tsx
@@ -3,10 +3,16 @@ import Messages from "@/app/login/messages";
 export const dynamic = "force-dynamic";
 
 type ServiceFormProps = {
-  services: Array<string>[] | undefined;
+  services: { name: string; price: number }[] | undefined;
 };
 
 const ServiceForm = ({ services }: ServiceFormProps) => {
+  const mappedServices =
+    services?.map((service) => (
+      <option key={service.name} value={service.name}>
+        {service.name} {service.price}kr
+      </option>
+    )) ?? [];
   return (
     <form method="post">
       <div className="flex-1 flex flex-col w-screen justify-center items-center gap-2 text-foreground px-4 text-black dark:text-white">
@@ -17,10 +23,9 @@ const ServiceForm = ({ services }: ServiceFormProps) => {
               name="services"
               required
             >
-              {services?.map((service) => (
-                <option value={service}>{service}</option>
-              ))}
+              {mappedServices}
             </select>
+            <div></div>
           </div>
           <button
             className={`bg-green-700 px-4 py-2 mb-2 w-[250px] text-white`}
