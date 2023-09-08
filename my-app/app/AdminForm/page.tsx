95b945a62b3fff4af0b2b2a3398d199a46b23b37
Thomas Danielsson <69715206+DanielssonThomas@users.noreply.github.com>
2023-09-08 09:12:04 +0200

    fix: removed console.log

--- my-app/app/AdminForm/page.tsx
+++
@@ -1,7 +0,0 @@
-import AdminForm from "../Admin/SubscriptionForm/page";
-
-function FormPage() {
-  return <AdminForm />;
-}
-
-export default FormPage;
--- my-app/components/LightSwitch.tsx
+++ my-app/components/LightSwitch.tsx
@@ -7,10 +7,7 @@ const LightSwitch = ({ lightMode, setLightMode }: LightSwitchProps) => {
   return (
     <button
       className="absolute top-4 right-4 dark:text-white text-black"
-      onClick={() => {
-        setLightMode(!lightMode);
-        console.log(lightMode);
-      }}
+      onClick={() => setLightMode(!lightMode)}
     >
       Light / Dark
     </button>
