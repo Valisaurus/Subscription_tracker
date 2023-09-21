a7540f2153c8920b1ab4e7e6fbe12d671c2deddb
Valisaurus <valley.alosachi@hotmail.com>
2023-09-21 11:21:21 +0200

    added dynamic

--- my-app/app/notifications/sendNotification/route.ts
+++ my-app/app/notifications/sendNotification/route.ts
@@ -3,6 +3,7 @@ import webpush from "web-push";
 import { createRouteHandlerClient } from "@supabase/auth-helpers-nextjs";
 import { cookies } from "next/headers";
 
+export const dynamic = "force-dynamic";
 export async function GET(_: NextRequest) {
   console.log("REACHED");
   const supabase = createRouteHandlerClient({ cookies });
