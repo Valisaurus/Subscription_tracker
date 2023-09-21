b8b904b84737c83417072a80b82629fa2e7dd601
Thomas Danielsson <69715206+DanielssonThomas@users.noreply.github.com>
2023-09-21 13:09:48 +0200

    fix: settings color modes

--- my-app/components/SettingsButton.tsx
+++ my-app/components/SettingsButton.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 const SettingsButton = () => {
   return (
-    <div className="absolute bottom-[63px] left-[160px]">
+    <div className="absolute bottom-[63px] left-[160px] text-black dark:text-white">
       <Link href="/settings">Settings</Link>
     </div>
   );
