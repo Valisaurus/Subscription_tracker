333077fbdbe22c30aba8943bdc381c688a9245b9
Thomas Danielsson <69715206+DanielssonThomas@users.noreply.github.com>
2023-09-26 13:48:28 +0200

    add: checkbox styling

--- my-app/components/ServiceForm.tsx
+++ my-app/components/ServiceForm.tsx
@@ -25,12 +25,12 @@ const ServiceForm = ({
     )) ?? [];
   return isVisible ? (
     <div>
-      <form action="services/addService" method="post">
+      <form action="services/addService" method="post" className="focus:ring-0">
         <div className="flex-1 flex flex-col w-screen justify-center items-center text-foreground px-4 text-black dark:text-white text-[20px]">
           <div className="flex flex-col justify-center items-center">
             <div className="w-[300px]">
               <select
-                className="px-4 border-4 w-full h-[45px] border-black bg-inherit text-black dark:border-white dark:bg-black dark:text-white mb-[10px] "
+                className="px-4 border-4 w-full h-[48px] border-black bg-inherit text-black dark:border-white dark:bg-black dark:text-white mb-[10px] "
                 name="subscription_id"
                 required
               >
@@ -39,18 +39,30 @@ const ServiceForm = ({
                 </option>
                 {mappedServices}
               </select>
-              <div>
+              <div className="flex items-center justify-between h-[48px] mb-[16px]">
                 <label
                   htmlFor="has_trial"
                   className="text-black dark:text-white"
                 >
-                  Trial period?
+                  <i>Trial period?</i>
                 </label>
-                <input
-                  type="checkbox"
-                  name="has_trial"
-                  onChange={() => setTrial(!trial)}
-                />
+                <div className="relative">
+                  <input
+                    type="checkbox"
+                    name="has_trial"
+                    onChange={() => setTrial(!trial)}
+                    className="appearance-none contents-['t'] checked:text-black checked:bg-white checked:dark:bg-black checked:dark:text-white w-[53px] h-[48px] border-4 border-black dark:border-white border-solid z-10"
+                  />
+                  <span
+                    className={`absolute top-[7px] left-[8px] h-[30px] w-[30px] text-black z-0 ${
+                      trial ? "block" : "hidden"
+                    } bg-black dark:bg-white`}
+                    style={{
+                      clipPath:
+                        "polygon(49% 66%, 85% 31%, 92% 37%, 50% 80%, 24% 56%, 31% 50%)",
+                    }}
+                  ></span>
+                </div>
               </div>
               {trial ? (
                 <div>
@@ -59,12 +71,12 @@ const ServiceForm = ({
                     className="text-black dark:text-white"
                   >
                     Enter end of trial date:
+                    <input
+                      className="px-4 border-4 w-full border-black bg-inherit text-black dark:border-white dark:bg-black dark:text-white mb-[10px] h-[48px]"
+                      type="date"
+                      name="trial_end_date"
+                    />
                   </label>
-                  <input
-                    className="px-4 border-4 w-full border-black bg-inherit text-black dark:border-white dark:bg-black dark:text-white mb-[10px] h-[45px]"
-                    type="date"
-                    name="trial_end_date"
-                  />
                 </div>
               ) : (
                 <div></div>
